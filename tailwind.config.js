9d26d8f5c01d509373eec9250d52775326de05bd
DeadSpark <deadspark143@gmail.com>
2022-06-24 22:19:35 +0530

    implemented tailwind support

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  content: [
+    './index.html',
+  ],
+
+  theme: {
+    extend: {
+
+      fontSize: {
+
+        '1sxl': '9rem',
+        '2sxl': '10rem',
+        '3sxl': '11rem',
+
+      }
+
+    },
+  },
+
+}
